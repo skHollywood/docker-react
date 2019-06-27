979307d5d27d104752f41c48c0f04626d59186a4
osboxes.org <siva.business99@gmail.com>
2019-06-27 13:31:20 -0400

    second commit

---
+++ create-git.sh
@@ -0,0 +1,6 @@
+echo "# docker-react" >> README.md
+git init
+git add README.md
+git commit -m "initial commit"
+git remote add origin https://github.com/skHollywood/docker-react.git
+git push -u origin master
